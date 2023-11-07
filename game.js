40e0f2d8a32025997c97571ad7f1b55f45bddd04
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-07 11:30:03 +0100

    lagt till metoden runround

--- game.js
+++ game.js
@@ -16,4 +16,24 @@ export default class Game {
     this.runRound()
   }
 
+
+  runRound() {
+    // create a new gallows
+    this.gallows = new Gallows()
+    // 10. ask for secret word                         bass
+    let question = new Question("Type the secret word, don't show your opponent: ")
+    this.secretWord = new SecretWord(question.answer)
+    print("The secret word has " + this.secretWord.length + " letters")
+    // process secret word into chars                  b a s s
+    // store found word as empty positions for chars   _ _ _ _
+    this.foundWord = new FoundWord(this.secretWord)
+    print(this.foundWord.asString)
+    // 20. ask for letter ? 
+    this.guessWord()
+    // loose round
+    // win round
+    // goto 10
+    // this.runRound()
+  }
+
 }
