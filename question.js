05fc6773e1432766360f0595bbdbd4780dab572e
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-07 11:19:52 +0100

    skapat klass question

--- question.js
+++ question.js
@@ -1,3 +1,12 @@
 import Module from "node:module"
 const require = Module.createRequire(import.meta.url)
 const prompt = require('prompt-sync')({ sigint: true })
+
+export default class Question {
+
+  constructor(questionText) {
+    this.questionText = questionText
+    this.answer = prompt(questionText).toUpperCase()
+  }
+
+}
