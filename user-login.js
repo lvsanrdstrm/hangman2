ecc6064df2b6a7b08c8555a2bfcd33581322686d
lvsanrdstrm <lvsanrdstrm@gmail.com>
2023-11-08 15:05:41 +0100

    tycker att createuser passar bättre men de funka inte så gav upp

--- user-login.js
+++ user-login.js
@@ -1,7 +1,21 @@
+import { appendFileSync, readFileSync } from 'node:fs'
 import PromptSync from 'prompt-sync'
 import User from "./user.js"
 
+const print = console.log
 
+const prompt = PromptSync({ sigint: true })
+
+let dbUsers = readFileSync("data/users.csv", "utf8")
+
+dbUsers = dbUsers.trim().split("\r\n")
+
+let users = []
+
+for (let dbUser of dbUsers) {
+  dbUser = dbUser.split(",")
+  users.push(new User(dbUser[0], dbUser[1]))
+}
 
 export default class UserLogIn {
 
@@ -31,7 +45,16 @@ export default class UserLogIn {
       }
     }
   }
-
+  createUser() {
+    this.username = prompt("please enter a username: ")
+    this.password = prompt("please enter a password: ")
+    appendFileSync('data/users.csv', this.username + ',' + this.password + '\n', 'utf8')
+    users.push(new User(this.username, this.password))
+    // tror inte ja behöver detta om ja appendar till users.csv 
+    // users.push(new User(username, password))
+    print("you're now a registred user. please log in: ")
+    this.logInUser()
+  }
 
 }
 
--- user.js
+++ user.js
@@ -1,21 +1,3 @@
-import PromptSync from 'prompt-sync'
-import { appendFileSync, readFileSync } from 'node:fs'
-
-const print = console.log
-
-const prompt = PromptSync({ sigint: true })
-
-let dbUsers = readFileSync("data/users.csv", "utf8")
-
-dbUsers = dbUsers.trim().split("\r\n")
-
-let users = []
-
-for (let dbUser of dbUsers) {
-  dbUser = dbUser.split(",")
-  users.push(new User(dbUser[0], dbUser[1]))
-}
-
 export default class User {
   name
   #password
@@ -27,17 +9,6 @@ export default class User {
 
   checkCredentials(username, password) {
     return this.name === username && this.#password === password
-  }
-
-  createUser() {
-    this.username = prompt("please enter a username: ")
-    this.password = prompt("please enter a password: ")
-    appendFileSync('data/users.csv', this.username + ',' + this.password + '\n', 'utf8')
-    this.users.push(new User(this.username, this.password))
-    // tror inte ja behöver detta om ja appendar till users.csv 
-    // users.push(new User(username, password))
-    print("you're now a registred user. please log in: ")
-    this.logInUser()
   }
 
 }
